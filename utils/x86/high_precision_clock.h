1d4d9abd3b1c148979a68e7b489b7ea433f26690
rohanverma94 <rohan40594@gmail.com>
2022-12-09 22:19:38 +0530

    ADDED High Precision Clock

--- utils/x86/high_precision_clock.h
+++ utils/x86/high_precision_clock.h
@@ -5,10 +5,17 @@
 #ifndef EMBEDRT_HIGH_PRECISION_CLOCK_H
 #define EMBEDRT_HIGH_PRECISION_CLOCK_H
 
+#include <cstdint>
+#include <atomic>
+#include <pthread.h>
 
 class high_precision_clock {
+    static std::atomic<int64_t> timer_count_micros;
+    static constexpr uint64_t kPrecisionMicros = 50;
+    static constexpr uint32_t kNumMicrosPerSecond = 100000;
 
-
+    static void* UpdateTimerCounter(void* arg);
+    pthread_t InitCacheClock();
 };
 
 
